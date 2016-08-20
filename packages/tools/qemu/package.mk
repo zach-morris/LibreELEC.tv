4f121cdb33c3b904ac429aa89e318d0153fa3e80
escalade <trondah@gmail.com>
2016-08-20 12:09:15 +0200

    qemu: update to 2.7.0-rc3

--- packages/tools/qemu/package.mk
+++ packages/tools/qemu/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="qemu"
-PKG_VERSION="2.7.0-rc1"
+PKG_VERSION="2.7.0-rc3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
