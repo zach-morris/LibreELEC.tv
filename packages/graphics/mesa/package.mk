c67fba804d60ceed1ce8b136b582679d3c8d5a7d
escalade <trondah@gmail.com>
2016-08-26 06:59:17 +0000

    mesa: update to 219a451

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mesa"
-PKG_VERSION="2301705"
+PKG_VERSION="219a451"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
