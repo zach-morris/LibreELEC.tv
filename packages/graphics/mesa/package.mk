542f0ca42f5e19296bb62c42577ad0ac4c57f2e2
escalade <trondah@gmail.com>
2016-09-15 20:55:57 +0000

    mesa: update to 0d2eb8c

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mesa"
-PKG_VERSION="cf7be70"
+PKG_VERSION="0d2eb8c"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
