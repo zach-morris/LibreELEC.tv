5e6775797d2bf417bc02cc4250bbd2134ef0f979
escalade <trondah@gmail.com>
2016-10-04 15:05:28 +0200

    mesa: update to 338d3c0

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mesa"
-PKG_VERSION="df92036"
+PKG_VERSION="338d3c0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
