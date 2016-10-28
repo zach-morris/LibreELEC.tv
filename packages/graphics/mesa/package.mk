2ad7fbaaea705a49b9e96b69652229b62148f650
escalade <trondah@gmail.com>
2016-10-28 20:47:29 +0200

    mesa: update to e6aeeac

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mesa"
-PKG_VERSION="2e423ca"
+PKG_VERSION="e6aeeac"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
