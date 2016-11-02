cd05414e3acbba7995f2cbacadabc2262db69c74
escalade <trondah@gmail.com>
2016-11-02 10:06:29 +0000

    mesa: update to 9f0726f and add userspace relocations patch

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mesa"
-PKG_VERSION="e6aeeac"
+PKG_VERSION="9f0726f"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
