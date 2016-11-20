82b7926f4d5c7d5bcec37778d4ef20591fcaae4b
escalade <trondah@gmail.com>
2016-11-20 13:30:18 +0100

    libgdiplus: don't use gold linker and modify for target install

--- packages/addons/addon-depends/mono-depends/libgdiplus/package.mk
+++ packages/addons/addon-depends/mono-depends/libgdiplus/package.mk
@@ -36,6 +36,5 @@ PKG_CONFIGURE_OPTS_TARGET="--enable-shared \
                            --with-libjpeg  \
                            --with-libtiff"
 
-makeinstall_target() {
-  make install DESTDIR=$INSTALL
-}
+pre_configure_target() {
+  strip_gold
