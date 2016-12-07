ff58d5d9589896e5887a335fc3ff91d954810750
escalade <trondah@gmail.com>
2016-12-07 23:40:03 +0000

    mesa: update to e9133dd

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mesa"
-PKG_VERSION="048143b"
+PKG_VERSION="e9133dd"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
