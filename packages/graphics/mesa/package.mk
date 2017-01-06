5d80e6a9cfb951296556b3dd6db5aec14d9503c0
escalade <trondah@gmail.com>
2017-01-06 23:24:35 +0100

    mesa: update to 1472ff3

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mesa"
-PKG_VERSION="ac57bcd"
+PKG_VERSION="1472ff3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
