e3a0be08de79dc614bcb89d6fa16694bb9067c2a
escalade <trondah@gmail.com>
2017-01-16 11:06:00 +0000

    linux: update to 4.10-rc4

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -58,7 +58,7 @@ case "$LINUX" in
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET imx6-status-led imx6-soc-fan irqbalanced"
     ;;
   *)
-    PKG_VERSION="4.10-rc3"
+    PKG_VERSION="4.10-rc4"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/testing/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
