f4f7476e1b0557787d4a2e5c094a98c05131d19f
escalade <trondah@gmail.com>
2017-02-27 22:02:34 +0000

    kodi: update to 97146a1

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="a10c504"
+PKG_VERSION="97146a1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
